814195edf001b624020c9e3f0254a160b870d9ff
Nadine Burkert <nadine.burkert@trivadis.com>
2019-11-06 12:05:05 +0100

    Test Commit Nadine

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -14,8 +14,8 @@ public class Main extends Application {
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+        //test commit Nadine
     }
-
 
     public static void main(String[] args) {
         launch(args);
