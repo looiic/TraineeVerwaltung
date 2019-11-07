707f2685fb2e914405e2021dad2267f94f06bb42
Loïc Jermann <loic.jermann@gmail.com>
2019-11-07 14:06:42 +0100

    Add Connector Class to connect to the DB
    Also with sample Code in Person

---
+++ src/main/java/logic/Connector.java
@@ -0,0 +1,37 @@
+package logic;
+
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public final class Connector {
+
+    private static Connection conn;
+
+
+    private Connector(){
+
+    }
+
+    /**
+     * Gibt Connection zurueck
+     * @return
+     * @throws Exception
+     */
+    public static Connection getConn() throws SQLException{
+        if (conn == null){
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/trainee_verwaltung",
+                    "root", "root");
+        }
+        return conn;
+    }
+
+    public static void closeConn() throws SQLException {
+        if(conn != null){
+            conn.close();
+        }
+    }
+
+}
