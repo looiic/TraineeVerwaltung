97a6b13ef1a4b7b7d29ee1c3cc84d3912e401de0
Nadine Burkert <nadine.burkert@trivadis.com>
2019-11-08 11:38:15 +0100

    bind kursInfo controller with elements

--- src/main/java/sample/KursInfoCtrl.java
+++ src/main/java/sample/KursInfoCtrl.java
@@ -1,4 +1,21 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import logic.Kurs;
+
 public class KursInfoCtrl {
+
+    @FXML TextField kursField;
+    @FXML TextField kursId;
+    @FXML TextField raumField;
+    @FXML TextField anzahlTN;
+
+    @FXML
+    public void setKursInfos(Kurs kurs) {
+        kursField.setText(kurs.getJahrgang());
+        kursId.setText(Integer.toString(kurs.getId()));
+        raumField.setText(kurs.getRaum());
+        anzahlTN.setText("tbd");
+    }
 }
