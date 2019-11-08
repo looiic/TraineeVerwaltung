2de768992cb2710882030921548c37ba6d0d59a7
Loïc Jermann <loic.jermann@gmail.com>
2019-11-08 11:19:45 +0100

    Show Kurse from DB in GUI

--- src/main/java/sample/KursListeCtrl.java
+++ src/main/java/sample/KursListeCtrl.java
@@ -1,20 +1,31 @@
 package sample;
 
+import DatenbankMethoden.DbKurs;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.ListView;
+import javafx.scene.control.TableView;
+import logic.Kurs;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class KursListeCtrl {
 
-    @FXML private ListView kursList;
+    @FXML private TableView kursTabelle;
 
     @FXML
     public void initialize(){
 
-        ObservableList<String> obsList = FXCollections.observableArrayList (
-                "Test1", "Test2", "Test3");
-        kursList.setItems(obsList);
+        DbKurs dbKurs = new DbKurs();
+        try {
+            ArrayList<Kurs> kursListe = dbKurs.getKursListe();
+            ObservableList<Kurs> obsList = FXCollections.observableArrayList(kursListe);
+            kursTabelle.setItems(obsList);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
 
     }
 
