ccbca1d4dba16b613e4f7457d2e5ba473282da5a
Oliver <oliver.wack@trivadis.com>
2019-11-11 14:00:40 +0100

    Methoden für jeden Kontroller der alle eigenen interaktiven Komponenten setDisable(bool). editTrainee Button: Sperrt alle Felder und aktiviert alle Felder bei TraineeInfo

--- src/main/java/sample/TraineeListeCtrl.java
+++ src/main/java/sample/TraineeListeCtrl.java
@@ -2,6 +2,8 @@ package sample;
 
 import DatenbankMethoden.DbKurs;
 import DatenbankMethoden.DbPerson;
+import javafx.beans.binding.Bindings;
+import javafx.beans.value.ChangeListener;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.Event;
@@ -9,6 +11,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
+import javafx.scene.control.ToggleButton;
 import logic.Kurs;
 import logic.Person;
 
@@ -24,17 +27,25 @@ public class TraineeListeCtrl {
     private Button editTrainee;
     @FXML
     private Label editTraineeInfo;
+    @FXML
+    private ToggleButton addTrainee;
+
 
     DbPerson dbPerson;
 
 
+    private Person selectedPerson;
+
+
     @FXML
     public void initialize() {
-
-
+        editTrainee.disableProperty().bind(Bindings.isEmpty(tableView.getSelectionModel().getSelectedItems()));
+        //tableView.getSelectionModel().selectedIndexProperty().getValue().bind();
         dbPerson = new DbPerson();
+        DbKurs dbKurs = new DbKurs();
+
         try {
-            ArrayList<Person> personenListe = dbPerson.getListPersonen();
+            ArrayList<Person> personenListe = dbPerson.getListPersonen(dbKurs.getKursListe().get(0));
             ObservableList<Person> obsList = FXCollections.observableArrayList(personenListe);
             tableView.setItems(obsList);
         } catch (SQLException e) {
@@ -61,37 +72,59 @@ public class TraineeListeCtrl {
         System.out.println(e);
         Object selectedItem = tableView.getSelectionModel().getSelectedItem();
         if (selectedItem instanceof Person) {
-            Person selectedPerson = (Person) selectedItem;
+            selectedPerson = (Person) selectedItem;
             TraineeInfoCtrl personInfoCtrl = ControllerManager.getTraineeInfoCtrl();
             personInfoCtrl.setTraineeInfos(selectedPerson);
-
-            editTrainee.setDisable(false);
         }
     }
+
+    /**
+     * Erstellt einen neues Trainee-Objekt. Alles weitere passiert später
+     */
+    @FXML
+    public void addTrainee(Event e) throws SQLException {
+        setEnabledState();
+    }
+
+
     @FXML
     public void editTrainee(Event e) {
         Object selectedItem = tableView.getSelectionModel().getSelectedItem();
         if (selectedItem instanceof Person) {
             setTraineeListDisabled(true);
+            tableView.getSelectionModel().clearSelection();
 
-            TraineeInfoCtrl personInfoCtrl = ControllerManager.getTraineeInfoCtrl();
-            personInfoCtrl.setTraineeInfoDisabled(false);
-
-            KursInfoCtrl kursInfoCtrl = ControllerManager.getKursInfoCtrl();
-            kursInfoCtrl.setKursInfoDisabled(true);
-
-            KursListeCtrl kursListeCtrl = ControllerManager.getKursListeCtrl();
-            kursListeCtrl.setKursListeDisabled(true);
+            setEnabledState();
 
         }
     }
 
+    private void setEnabledState() {
+        TraineeInfoCtrl personInfoCtrl = ControllerManager.getTraineeInfoCtrl();
+        personInfoCtrl.setTraineeInfoDisabled(false);
+
+        KursInfoCtrl kursInfoCtrl = ControllerManager.getKursInfoCtrl();
+        kursInfoCtrl.setKursInfoDisabled(true);
+
+        KursListeCtrl kursListeCtrl = ControllerManager.getKursListeCtrl();
+        kursListeCtrl.setKursListeDisabled(true);
+    }
+
     public void setTraineeListDisabled(boolean bool){
         tableView.setDisable(bool);
-        editTrainee.setDisable(bool);
+        addTrainee.setDisable(bool);
+        //editTrainee.setDisable(bool);
     }
 
     public void clearTable() {
         tableView.getItems().clear();
+    }
+
+    public Person getSelectedPerson() {
+        return selectedPerson;
+    }
+
+    public ToggleButton getAddTrainee() {
+        return addTrainee;
     }
 }
