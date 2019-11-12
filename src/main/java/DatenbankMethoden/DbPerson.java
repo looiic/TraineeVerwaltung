1613d11198a4e6fb683c367fe6534e52857eadbb
Jankomo <Jankomo@gmx.de>
2019-11-12 12:03:05 +0100

    Methode getListPersonen entfernt, da ungenutzt bzw. unnötig.

--- src/main/java/DatenbankMethoden/DbPerson.java
+++ src/main/java/DatenbankMethoden/DbPerson.java
@@ -41,34 +41,6 @@ public class DbPerson {
         return personen;
     }
 
-    /**
-     * Lade alle Personen..vllt unnötig
-     *
-     * @return
-     * @throws SQLException
-     */
-    public ArrayList<Person> getListPersonen() throws SQLException {
-        Statement stmt = null;
-        String query =
-                "select id, vorname, nachname, " +
-                        "standort, vorkenntnisse, kurs_id " +
-                        "from trainee_verwaltung.Person";
-
-        ArrayList<Person> personen = new ArrayList<>();
-        stmt = Connector.getConn().createStatement();
-        ResultSet rs = stmt.executeQuery(query);
-        while (rs.next()) {
-            int id = rs.getInt("id");
-            String vorname = rs.getString("vorname");
-            String nachname = rs.getString("nachname");
-            String standort = rs.getString("standort");
-            String vorkenntnisse = rs.getString("vorkenntnisse");
-            int kurs_id = rs.getInt("kurs_id");
-            personen.add(new Person(id, vorname, nachname, standort, vorkenntnisse, kurs_id));
-        }
-        stmt.close();
-        return personen;
-    }
 
     public int addNewPerson(Person person) throws SQLException {
         int newPersonId = 0;
