e066fea5f0753d10e85eebe9ac6646a04c81a125
Loïc Jermann <loic.jermann@gmail.com>
2019-11-12 14:32:24 +0100

    Move Connector Class to package DatenbankMethoden

--- src/main/java/DatenbankMethoden/Connector.java
+++ src/main/java/DatenbankMethoden/Connector.java
@@ -1,8 +1,12 @@
-package logic;
+package DatenbankMethoden;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 
 public final class Connector {
@@ -10,17 +14,18 @@ public final class Connector {
     private static Connection conn;
 
 
-    private Connector(){
+    private Connector() {
 
     }
 
     /**
      * Gibt Connection zurueck
+     *
      * @return
      * @throws Exception
      */
-    public static Connection getConn() throws SQLException{
-        if (conn == null){
+    public static Connection getConn() throws SQLException {
+        if (conn == null) {
             conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/trainee_verwaltung",
                     "root", "root");
         }
@@ -28,7 +33,7 @@ public final class Connector {
     }
 
     public static void closeConn() throws SQLException {
-        if(conn != null){
+        if (conn != null) {
             conn.close();
             conn = null;
         }
